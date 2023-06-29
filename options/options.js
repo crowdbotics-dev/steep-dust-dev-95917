0ca3433bcc454a6aad8688141ae271ab1f2019a4
Crowdbotics <engineering@crowdbotics.com>
2023-06-29 10:36:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steep-dust-dev-95917",
+  url: "https://steep-dust-dev-95917.botics.co",
+  api: "https://steep-dust-dev-95917.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
